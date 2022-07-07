02b35cc4033232481d55d5aeb188ef22e3ebe1c5
Benjamin Correa <benjamin.c@smartwaystudio.com>
2022-07-07 14:46:33 -0300

    Add EF Core

---
+++ WebAppEfCore/BusinessDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppEfCore.Entities;
+
+namespace WebAppEfCore
+{
+    public class BusinessDbContext : DbContext
+    {
+        public BusinessDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WebAppEfCore/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppEfCore.Entities
+{
+    public class Product : BaseEntity
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [ConcurrencyCheck]
+        public string Name { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+    }
+}
